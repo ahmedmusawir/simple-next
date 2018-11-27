bdedabadb11c4a60fb0ce7244682bb4f54ff1202
Da Moose <odesk.shourav@gmail.com>
2018-11-27 17:57:37 +0800

    Simple Next w/ routing, fetch and NProgress - 27 Nov 2018

---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const HireMe = () => (
+  <Layout title="Hire Me">
+    <p>
+      You can hire me at{" "}
+      <a href="mailto:moose@email.com">moose@email.com</a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
